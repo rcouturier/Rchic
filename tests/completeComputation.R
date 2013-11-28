978ce2b644b9fe1eecc506ac30cbf6d6b53b74d7
couturie <couturie@extinction>
2013-11-28 19:50:13 +0100

    add of fileselect

--- tests/completeComputation.R
+++ tests/completeComputation.R
@@ -2,24 +2,39 @@
 #############    TO BE CHANGED    ############
 ##############################################
 library(rchic)
-#workingDirectory='/home/couturie/Rchic/R'
-workingDirectory='C:\\Documents and Settings\\coutu\\Mes documents\\Rchic\\R'
+require(tcltk)
+
+workingDirectory='/home/couturie/Rchic/R'
+#workingDirectory='C:\\Documents and Settings\\coutu\\Mes documents\\Rchic\\R'
 
 
 setwd(workingDirectory)
 
 
-#loading of all functions
-source('data2transac.R')
-source('callAsirules.R')
-source('readRulesAndDisplay.R')
-# 
-# 
-# 
-# #read of the file
-dataCSV<-read.csv('../tests/animaux.csv',sep=";")
-# 
-# 
- data2transac(dataCSV)
- callAsirules()
- readRulesAndDisplay()
+fileName <- tclvalue(tkgetOpenFile()) # Very simple, isn't it?
+
+if (!nchar(fileName)) {
+  tkmessageBox(message = "No file was selected!")
+} else {
+  
+  #tkmessageBox(message = paste("The file selected was", dirname(fileName)))
+  #tkmessageBox(message = paste("The file selected was", basename(fileName)))
+  
+  
+  #loading of all functions
+  source('data2transac.R')
+  source('callAsirules.R')
+  source('readRulesAndDisplay.R')
+  # 
+  # 
+  # 
+  # #read of the file
+  dataCSV<-read.csv(fileName,sep=";")
+  # 
+  # 
+  data2transac(dataCSV)
+  callAsirules()
+  readRulesAndDisplay()
+  
+}
+
