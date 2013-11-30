745817b364eb04aef3e897475b98a84e7358bd7a
couturie <couturie@extinction>
2013-11-30 09:08:47 +0100

    remove useless comments

--- tests/completeComputation.R
+++ tests/completeComputation.R
@@ -4,37 +4,27 @@
 library(rchic)
 require(tcltk)
 
-#workingDirectory='/home/couturie/Rchic/R'
-#workingDirectory='C:\\Documents and Settings\\coutu\\Mes documents\\Rchic\\R'
-#initDirectory=getwd()
-#workingDirectory= paste(getwd(),'/R',sep="")
-#setwd(workingDirectory)
+
 source('R/data2transac.R')
 source('R/callAsirules.R')
 source('R/readRulesAndDisplay.R')
-#setwd(initDirectory) 
 
 
-fileName <- tclvalue(tkgetOpenFile()) # Very simple, isn't it?
+
+#select file
+fileName <- tclvalue(tkgetOpenFile())
 
 if (!nchar(fileName)) {
   tkmessageBox(message = "No file was selected!")
   setwd(initDirectory)
 } else {
   
-  #tkmessageBox(message = paste("The file selected was", dirname(fileName)))
-  #tkmessageBox(message = paste("The file selected was", basename(fileName)))
-  
+ 
   
   #loading of all functions
   
-  # 
-  # 
-  # 
-  # #read of the file
+  #read of the file
   dataCSV<-read.csv(fileName,sep=";")
-  # 
-  # 
   data2transac(dataCSV)
   callAsirules()
   readRulesAndDisplay()
