86aca553d37d2f06f22f73c4508b45838a3a8ad4
raphael couturier <couturie@extinction>
2014-06-29 17:15:12 +0200

    first version of movable node (text) and splines

--- R/implicativeGraph.R
+++ R/implicativeGraph.R
@@ -175,27 +175,23 @@ plotImplicativeGraph <- function(thres=99,value,cbvalue,color,list.selected.item
   #get the list of nodes
   nodes = AgNode(graph1)
   
+  #coordinates to compute the delta of a move from the last mouse move
   lastX=0
   lastY=0
+  #coordinates to compute the delta of a move from the initial button pressed
+  initX=0
+  initY=0
   
   list.spline <<- list()
   list.node <- list()
-  r=3
   
-  moveDown <- function(i) {
+  #size of the controle nodes
+  r=5
+
+  #mouse pressed on text item
+  text_pressed <- function(i) {
     force(i)
-    
     function(x,y){
-      
-      ## This procedure is invoked when the mouse is pressed over one
-      ## of the data points.  It sets up state to allow the point
-      ## to be dragged.
-      ##
-      ## Arguments:
-      ## x, y -  The coordinates of the mouse press.
-      print("movedown")
-      print(x)
-      print(y)
       x <- as.numeric(x)
       y <- as.numeric(y)
       tkdtag(canvas, "selected")
@@ -203,25 +199,16 @@ plotImplicativeGraph <- function(thres=99,value,cbvalue,color,list.selected.item
       tkitemraise(canvas,"current")
       lastX <<- x
       lastY <<- y
+      initX <<- x
+      initY <<- y
     }
   }
-  
-  moveNode <- function(i)
+
+  #mouse move on the text
+  text_move <- function(i)
   {
     force(i)
-    
     function(x,y){
-      
-      ## This procedure is invoked during mouse motion events.
-      ## It drags the current item.
-      ##
-      ## Arguments:
-      ## x, y -  The coordinates of the mouse.
-      print("movenode")
-      print(x)
-      print(y)
-      print(lastX)
-      print(lastY)
       x <- as.numeric(x)
       y <- as.numeric(y)
       tkmove(canvas, "selected", x-lastX,y-lastY)
@@ -230,54 +217,53 @@ plotImplicativeGraph <- function(thres=99,value,cbvalue,color,list.selected.item
     }
   }
   
-  moveEdges <- function(i){
+  #mouse release on the text
+  text_released <- function(i){
     force(i)
     function(x,y) {
-      print("move edge3")
-      #print("from")
-      #print(list.from[[i]])
+      #x and y contain the delta move from the button pressed
+      x=as.numeric(x)-initX
+      y=as.numeric(y)-initY
+      #for all edges from this node
       for(e in list.from[[i]]) {
         sp=list.spline[[e]]
-        sp$coord[[1]]=as.numeric(x)
-        sp$coord[[2]]=as.numeric(y)+15
-        print(list.spline)
+        sp$coord[[1]]=sp$coord[[1]]+as.numeric(x)
+        sp$coord[[2]]=sp$coord[[2]]+as.numeric(y)
+        #update the spline in the list
         list.spline[[e]]<<-sp
-        print(list.spline)
+        #update the spline on the canvas
         tkcoords(canvas,sp$spline,sp$coord)
       }
       
-      
-      #print("to")
-      #print(list.to[[i]])
+      #for all edges to this node
       for(e in list.to[[i]]) {
         sp=list.spline[[e]]
-        sp$coord[[7]]=as.numeric(x)
-        sp$coord[[8]]=as.numeric(y)-15
-        print(list.spline)
+        sp$coord[[7]]=sp$coord[[7]]+as.numeric(x)
+        sp$coord[[8]]=sp$coord[[8]]+as.numeric(y)#-15
+        #update the spline in the list
         list.spline[[e]]<<-sp
-        print(list.spline)
+        #update the spline on the canvas
         tkcoords(canvas,sp$spline,sp$coord)
       }
     }
   }
   
   
-  
-  control_point_selected <- function (i) {
+  #mouse pressed on control point
+  control_point_pressed <- function (i) {
     force(i)
     
     function(x,y){
-      print("select")
-      print(i)
       tkdtag(canvas, "selected")
       tkaddtag(canvas, "selected", "withtag", "current")
       tkitemraise(canvas,"current")
-      #tkaddtag ( canvas , "selected" , "withtag" , "current" )
-      #tkitemraise ( canvas , "current" )
       last_pos <<- as.numeric ( c ( x , y ) )
+      initX <<- as.numeric(x)
+      initY <<- as.numeric(y)
     }
   }
   
+  #mouse move on control point
   control_point_move <- function ( i ) {
     force(i)
     function(x,y) {
@@ -288,78 +274,92 @@ plotImplicativeGraph <- function(thres=99,value,cbvalue,color,list.selected.item
     }
   }
   
-  control_point_release <- function ( spline,pos) {
+  #mouse released ono control point
+  control_point_released <- function ( spline,pos) {
     force(spline)
     function(x,y){
-      print("release")
-      
-      print(spline) 
-      print(pos)
-      sp=list.spline[[spline]]
-      if(pos==2) {
-        sp$coord[[3]]=as.numeric(x)
-        sp$coord[[4]]=as.numeric(y)
+      #x and y contain the delta move from the button pressed
+      x=as.numeric(x)-initX
+      y=as.numeric(y)-initY
+      #when a control point between two splines, the first must be distinguished from the second
+      first=TRUE
+      for(s in spline) {
+        sp=list.spline[[s]]
+        #update of the second control point of the spline
+        if(pos==2) {
+          sp$coord[[3]]=sp$coord[[3]]+x
+          sp$coord[[4]]=sp$coord[[4]]+y
+        }
+        #update of the third control point of the spline
+        if(pos==3) {
+          sp$coord[[5]]=sp$coord[[5]]+x
+          sp$coord[[6]]=sp$coord[[6]]+y
+        }
+        #update of the fourth control point of the spline
+        if(pos==4 & first==TRUE) {
+          sp$coord[[7]]=sp$coord[[7]]+x
+          sp$coord[[8]]=sp$coord[[8]]+y
+          first=FALSE
+        }
+        else
+          #update of the first control point of the next spline
+          #because this control point is joining 2 splines
+          if(pos==4 & first==FALSE) {
+            sp$coord[[1]]=sp$coord[[1]]+x
+            sp$coord[[2]]=sp$coord[[2]]+y
+          }
+        #update the spline in the list
+        list.spline[[s]]<<-sp
+        #update the spline on the canvas
+        tkcoords(canvas,sp$spline,sp$coord)
       }
-      if(pos==3) {
-        sp$coord[[5]]=as.numeric(x)
-        sp$coord[[6]]=as.numeric(y)
-      }
-      list.spline[[spline]]<<-sp
-      tkcoords(canvas,sp$spline,sp$coord)
-      #tkdtag ( W , "selected" )
-     
     }
   }
   
+  #last position for control point
   last_pos <<- numeric( 2 )
   
+  #function to create a control point
   createPoint <- function(nb,x,y,r,spline,pos) {
     p <- tkcreate ( canvas , "oval" , x - r , y - r , x + r , y + r ,
-                       width = 1 , outline = "black" , fill = "blue",tags="draw" )
-    #tkaddtag ( canvas , "point2" , "withtag" , item )
+                    width = 1 , outline = "black" , fill = "blue",tags="draw" )
     
-    
-    
-    # global to track position
-     tkitembind ( canvas , p , "<1>" , control_point_selected(nb) )
-     tkitembind ( canvas , p,"<B1-Motion>" , control_point_move(nb) )
-     tkitembind ( canvas , p, "<ButtonRelease-1>" , control_point_release(spline,pos))
-    print("spline")
-    print(spline)
-    print(nb)
-    #tkbind ( canvas , "<ButtonRelease-1>" , function ( W ) tkdtag ( W , "selected" ))
-    
-    
-    
+    #functions to track the mouse
+    tkitembind ( canvas , p , "<1>" , control_point_pressed(nb) )
+    tkitembind ( canvas , p,"<B1-Motion>" , control_point_move(nb) )
+    tkitembind ( canvas , p, "<ButtonRelease-1>" , control_point_released(spline,pos))
   }
   
   
   
-  #nodeItem <-  vector("character",length(nodes))
-  
-  
-  
-  
+  #number of splines  
   nb.spline=1
+  #number of control points
   nb.control_point=1
   
   
   #if the list of nodes is not empty
   if(length(nodes)>0) {
-    #create the text of the nodes
+    #create texts for all the nodes
     for (i in 1:length(nodes)) {
+      #get current node
       node=nodes[[i]]
+      #get coordinate of current node
       coord=getNodeXY(node)
+      #get the name of the node
       name=name(node)
-      p<-tkcreate(canvas, "text", offsetX+coord$x*scalingFactorX, workingHeight+0-coord$y*scalingFactorY, text=name,font=plotFont, fill="brown",tags="draw")
       
-      tkitembind(canvas, p, "<1>", moveDown(i))
-      tkitembind(canvas, p,"<B1-Motion>", moveNode(i))
-      tkitembind(canvas, p, "<ButtonRelease-1>",moveEdges(i))
-    
+      #create the text
+      p<-tkcreate(canvas, "text", offsetX+coord$x*scalingFactorX, workingHeight+0-coord$y*scalingFactorY, text=name,font=plotFont, fill="brown",tags="draw")
+      #function to track the mouse
+      tkitembind(canvas, p, "<1>", text_pressed(i))
+      tkitembind(canvas, p,"<B1-Motion>", text_move(i))
+      tkitembind(canvas, p, "<ButtonRelease-1>",text_released(i))
+      #add the name in the list
       list.node=c(list.node,name)
     }
     
+    #we need 2 lists: one for the edges from the node and the other one for the edges to the node
     list.from = vector("list",length(nodes))  
     list.to = vector("list",length(nodes))  
     
@@ -369,24 +369,25 @@ plotImplicativeGraph <- function(thres=99,value,cbvalue,color,list.selected.item
     for (i in 1:length(edges)) {
       edge=edges[[i]]
       #for all splines
-      print(paste("edge",tail(edge),"->",head(edge)))
-      
       for (j in 1:numSplines(edge)) { 
         coord=getSpline(edge, j)
+        #if this is the last spline, we add an arrow
         if(j==numSplines(edge)) {
           arrow='last'
         }
         else {
           arrow='none'
         }
+        #compute the coordinates of the spline according to the scaling factor
         lCoord=numeric(8)
         for(k in 1:4) {
           lCoord[2*k-1]=offsetX+slot(cPoints(coord)[[k]],'x')*scalingFactorX
           lCoord[2*k]=workingHeight+0-slot(cPoints(coord)[[k]],'y')*scalingFactorY
         }
+        #get the value of this rule
         val=rules[paste(tail(edge),"->",head(edge)),5]
-        print(val)
         col="black"
+        #compute the color of the edge according to the value of the rule
         if(cbvalue[[1]]==1 && value[[1]]<val)
           col=color[[1]]
         else
@@ -398,32 +399,67 @@ plotImplicativeGraph <- function(thres=99,value,cbvalue,color,list.selected.item
         else
           if(cbvalue[[4]]==1 && value[[4]]<val)
             col=color[[4]]
+        #draw the spline
         sp=tkcreate(canvas, "line", lCoord,width=2,arrow=arrow,smooth='bezier',splinesteps=6,tags="draw",fill=col)
-        
+        #compute the number of the node from and to
         from=which(list.node==tail(edge))
         to=which(list.node==head(edge))
-        
+        #this list contains information on all the splines
         list.spline <<- c(list.spline,list(list(spline=sp,coord=lCoord,from=from,to=to)))
         
         
-        
-        if(j==1) {              # from node
+        #if this is the first spline of the edge
+        if(j==1) {            
+          #add the current spline to the list from for this node
           list.from[[from]]=c(list.from[[from]],nb.spline)
-          createPoint(nb=nb.control_point,x=lCoord[3],y=lCoord[4],r=r,spline=nb.spline,pos=2)
+          #create control points
+          #control point 2
+          createPoint(nb=nb.control_point,x=lCoord[3],y=lCoord[4],r=r,spline=c(nb.spline),pos=2)
+          nb.control_point=nb.control_point+1
+          #control point 3
+          createPoint(nb=nb.control_point,x=lCoord[5],y=lCoord[6],r=r,spline=c(nb.spline),pos=3)
+          nb.control_point=nb.control_point+1
+          #if there is another spline
+          if(numSplines(edge)!=1) {
+            #control point 4 and 1 for the next spline
+            #warning the field spline is a vector with the current and the next spline
+            createPoint(nb=nb.control_point,x=lCoord[7],y=lCoord[8],r=r,spline=c(nb.spline,nb.spline+1),pos=4)
+            nb.control_point=nb.control_point+1
+          }
+        }
+        #if this is the last spline of the edge
+        if(j==numSplines(edge)){ 
+          #add the current spline to the list to for this node
+          list.to[[to]]=c(list.to[[to]],nb.spline)
+          #if this spline is not the first one
+          if(j!=1) {
+            #control point 2
+            createPoint(nb=nb.control_point,x=lCoord[3],y=lCoord[4],r=r,spline=c(nb.spline),pos=2)
+            nb.control_point=nb.control_point+1
+            #control point 3
+            createPoint(nb=nb.control_point,x=lCoord[5],y=lCoord[6],r=r,spline=c(nb.spline),pos=3)
+            nb.control_point=nb.control_point+1
+          }
+        }
+        #if the spline is neither the first nor the last one
+        if(j!=1 && j!=numSplines(edge)) {
+          #control point 2
+          createPoint(nb=nb.control_point,x=lCoord[3],y=lCoord[4],r=r,spline=c(nb.spline),pos=2)
+          nb.control_point=nb.control_point+1
+          #control point 3
+          createPoint(nb=nb.control_point,x=lCoord[5],y=lCoord[6],r=r,spline=c(nb.spline),pos=3)
+          nb.control_point=nb.control_point+1
+          #control point 4 and 1 (see spline 1)
+          createPoint(nb=nb.control_point,x=lCoord[7],y=lCoord[8],r=r,spline=c(nb.spline,nb.spline+1),pos=4)
           nb.control_point=nb.control_point+1
           
-        }
-        if(j==numSplines(edge)){ # to node
-          list.to[[to]]=c(list.to[[to]],nb.spline)
-          createPoint(nb=nb.control_point,x=lCoord[5],y=lCoord[6],r=r,spline=nb.spline,pos=3)
-          nb.control_point=nb.control_point+1
         }
         nb.spline=nb.spline+1
       }
     }
   }
   
-    
+  
   #write the current postscript image in the current directory
   tkpostscript(canvas, file="graph.ps",height=workingHeight,width=workingWidth)
   
